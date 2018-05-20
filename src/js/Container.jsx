f3c99c36ef0abb97c7533f90dd990a1d776a9a4d
Dhara Shah <DharaShah@Dharas-MacBook-Air.local>
2018-05-20 20:07:05 +0530

    added accordian in mobile mode

--- src/js/Container.jsx
+++ src/js/Container.jsx
@@ -150,8 +150,44 @@ export default class toCreditsCard extends React.Component {
   onClickFirstExpand(e){
     document.querySelector(".col-16-tool-strip").style.height = "250px"
     document.querySelectorAll(".verticle-divider").forEach((d, i) => {
-      document.querySelectorAll(".verticle-divider")[i].style.height = "230px"
+      document.querySelectorAll(".verticle-divider")[i].style.height = "250px"
     })   
+  }
+
+  expandOnCard1Click(e){
+    document.querySelectorAll('.col-4-card-content').forEach((d, i) => {
+      document.querySelectorAll('.col-4-card-content')[i].style.display = "none";
+    })
+    document.querySelector("#card_1").style.display = "block";
+    document.querySelectorAll('.col-4-tool-card').forEach((d,i) => {
+      document.querySelectorAll('.col-4-tool-card')[i].style.height = 'auto'
+    })
+    document.querySelector('#card_1_tool').style.height = "250px"
+    document.querySelector('#card_1_tool').style.transition = "0.4s"
+  }
+
+  expandOnCard2Click(e){
+    document.querySelectorAll('.col-4-card-content').forEach((d, i) => {
+      document.querySelectorAll('.col-4-card-content')[i].style.display = "none";
+    })
+    document.querySelector("#card_2").style.display = "block";
+    document.querySelectorAll('.col-4-tool-card').forEach((d,i) => {
+      document.querySelectorAll('.col-4-tool-card')[i].style.height = 'auto'
+    })
+    document.querySelector('#card_2_tool').style.height = "250px"
+    document.querySelector('#card_2_tool').style.transition = "0.4s"
+  }
+
+  expandOnCard3Click(e) {
+    document.querySelectorAll('.col-4-card-content').forEach((d, i) => {
+      document.querySelectorAll('.col-4-card-content')[i].style.display = "none";
+    })
+    document.querySelector("#card_3").style.display = "block";
+    document.querySelectorAll('.col-4-tool-card').forEach((d,i) => {
+      document.querySelectorAll('.col-4-tool-card')[i].style.height = 'auto'
+    })
+    document.querySelector('#card_3_tool').style.height = "250px"
+    document.querySelector('#card_3_tool').style.transition = "0.4s"
   }
 
   renderCol16(){
@@ -267,7 +303,106 @@ export default class toCreditsCard extends React.Component {
         <div>Loading</div>
       )
     }else{
-      
+       let data = this.state.dataJSON.card_data.data,
+        drug_data = this.state.drug_data,
+        selectedOption = this.state.selectedOption,
+        card_1 = this.state.dataJSON.card_data.data.card_1,
+        card_2 = this.state.dataJSON.card_data.data.card_2,
+        card_3 = this.state.dataJSON.card_data.data.card_3,
+        card_4 = this.state.dataJSON.card_data.data.card_4;
+      console.log(drug_data, "drug data")
+      return(
+        <div className="col-4-tool-strip col-4-mobile">
+          <div id="card_1_tool" className="col-4-tool-card" onClick={(e) => this.expandOnCard1Click(e)}>
+            <div className="tool-card-title">
+              <img src="img/facilities-icon.png" />
+              {card_1.title}
+              <img src="img/down-arrow.png" className="down-arrow-icon"/>
+            </div>
+            <div id="card_1" className="col-4-card-content">
+              <p>Find the nearest health facilities around you</p>
+              <input type="text" name="area" className="area-input" placeholder="Start typing the name of your area" />
+              <div className="tool-call-to-action-area">
+                <div className="tool-call-to-action-button" onClick={(e) => this.getHealthFacilities(card_1)}>
+                  Search
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="horizontal-divider"></div>
+          <div id="card_2_tool" className="col-4-tool-card" onClick={(e) => this.expandOnCard2Click(e)}>
+            <div className="tool-card-title">
+              <img src="img/pharmacies-icon.png" />
+              {card_2.title}
+              <img src="img/down-arrow.png" className="down-arrow-icon" />
+            </div>
+            <div id="card_2" className="col-4-card-content">
+              <p>Find pharmacies</p>
+              <input type="text" name="location" className="location-input" placeholder="Search by location" />
+              <input type="text" name="pharmacie-name" className="pharmacie-name-input" placeholder="Search by pharmacie name" />
+              <div className="tool-call-to-action-area">
+                <div className="tool-call-to-action-button" onClick={(e) => this.getPharmacies(card_2)}>
+                  Search
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="horizontal-divider"></div>
+          <div id="card_3_tool" className="col-4-tool-card" onClick={(e) => this.expandOnCard3Click(e)}>
+            <div className="tool-card-title">
+              <img src="img/meds-icon.png" />
+              {card_3.title}
+              <img src="img/down-arrow.png" className="down-arrow-icon" />
+            </div>
+            <div id="card_3" className="col-4-card-content">
+              <p>How much do you pay for drugs?</p>
+              <div className="drug-name-dropdown">
+                <Select 
+                  name="drug_name"
+                  placeholder="Drug Name" 
+                  value={selectedOption['drug_name'] && selectedOption['drug_name'].value}
+                  onChange={(d)=>this.handleSelectOption(d,"drug_name")} 
+                  options={drug_data.drugs.map((d)=>{
+                    let temp = {};
+                    temp.label = d.name;
+                    temp.value = d.name;
+                    return temp;
+                  })}
+                />
+              </div>
+              <input type="text" name="dose" className="dose-input" placeholder="No. of dose" />
+              <input type="text" name="price" className="price-input" placeholder="Buying price" />
+              <div className="tool-call-to-action-area">
+                <div className="tool-call-to-action-button" onClick={(e) => this.getMedicinePrices(card_3)}>
+                  Search
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="horizontal-divider"></div>
+          <div className="col-4-tool-card">
+            <div className="tool-card-title">
+              <img src="img/eshangazi-icon.png" />
+              {card_4.title}
+            </div>
+            <div className="tool-call-to-action-area-talk">
+              <a href={card_4.url} target="_blank"><div className="tool-call-to-action-button-talk">
+                Talk
+              </div></a>
+            </div>
+          </div>
+          <Modal
+            showModal={this.state.showModal}
+            closeModal={this.closeModal}
+            mode={this.state.mode}
+            dataJSON={data}
+            responseData={this.state.responseData}
+            cardType={this.state.card_type}
+            title={this.state.title}
+            buying_price={this.state.buying_price}
+          />
+        </div>
+      )      
     }
   }
 
