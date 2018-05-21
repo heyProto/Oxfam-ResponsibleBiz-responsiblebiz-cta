425199e74686a48b9ef00ef93185ed2887044c2b
Dhara Shah <DharaShah@Dharas-MacBook-Air.local>
2018-05-21 23:39:35 +0530

    on click of modal overlay, close modal

--- src/js/Modal.js
+++ src/js/Modal.js
@@ -195,11 +195,15 @@ class Modal extends React.Component {
       <ReactModal
         isOpen={this.props.showModal}
         onAfterOpen={this.afterOpen}
+        onRequestClose={((e) => {
+          this.handleRequestClose(e);
+          this.props.closeModal(e);
+        })}
         closeTimeoutMS={0}
         overlayClassName="protograph-modal-overlay"
         className={`proto-col ${classname} protograph-modal`}
         shouldFocusAfterRender={false}
-        shouldCloseOnOverlayClick={false}
+        shouldCloseOnOverlayClick={true}
         shouldCloseOnEsc={false}
         shouldReturnFocusAfterClose={true}
         role="dialog"
